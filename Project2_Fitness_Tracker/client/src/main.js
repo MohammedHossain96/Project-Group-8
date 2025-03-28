25c4f2d6ff444cf202d34412c38fc6968dc3c6cf
Hunter <hunterwest94@rocketmail.com>
2025-03-27 22:27:39 -0400

    fixing errors

--- Project2_Fitness_Tracker/client/src/main.js
+++ Project2_Fitness_Tracker/client/src/main.js
@@ -1,28 +1,29 @@
+import { jsx as _jsx } from "react/jsx-runtime";
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
-import App from './App.tsx';
+import App from './App';
 import ErrorPage from './pages/ErrorPage.tsx';
 import Home from './pages/Home.tsx';
 import Login from './pages/Login.tsx';
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <App />,
-        errorElement: <ErrorPage />,
+        element: _jsx(App, {}),
+        errorElement: _jsx(ErrorPage, {}),
         children: [
             {
                 index: true,
-                element: <Home />,
+                element: _jsx(Home, {}),
             },
             {
                 path: '/login',
-                element: <Login />,
+                element: _jsx(Login, {}),
             },
         ],
     },
 ]);
 const rootElement = document.getElementById('root');
 if (rootElement) {
-    ReactDOM.createRoot(rootElement).render(<RouterProvider router={router}/>);
+    ReactDOM.createRoot(rootElement).render(_jsx(RouterProvider, { router: router }));
 }
