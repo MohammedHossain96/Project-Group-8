90264965b386ee3589347b1e7a033e9dc356367b
Hunter <hunterwest94@rocketmail.com>
2025-04-01 15:27:53 -0400

    adding badge feature

--- Project2_Fitness_Tracker/client/src/App.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import { Outlet } from 'react-router-dom';
-import Navbar from './components/Navbar';
-const App = () => {
-    return (_jsxs("div", { children: [_jsx(Navbar, {}), _jsx(Outlet, {}), " "] }));
-};
-export default App;
