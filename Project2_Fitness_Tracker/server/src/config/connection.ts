a07ea88a303f1e8508fc09632b6549a736e07711
Jen <jensavino26@gmail.com>
2025-04-02 21:44:32 -0400

    changes to connection.ts for render

--- Project2_Fitness_Tracker/server/src/config/connection.ts
+++ Project2_Fitness_Tracker/server/src/config/connection.ts
@@ -4,16 +4,22 @@ dotenv.config();
 import { Sequelize } from 'sequelize';
 
 const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
+  ? new Sequelize(process.env.DB_URL, {
+      dialect: 'postgres',
+      dialectOptions: {
+        ssl: process.env.DB_SSL === 'true' ? { require: true, rejectUnauthorized: false } : false,
+      },
+    })
   : new Sequelize(
       process.env.DB_NAME || '',
       process.env.DB_USER || '',
-      process.env.DB_PASSWORD,
+      process.env.DB_PASSWORD || '',
       {
-        host: 'localhost',
+        host: process.env.DB_HOST || 'localhost',
+        port: parseInt(process.env.DB_PORT || '5432', 10),
         dialect: 'postgres',
         dialectOptions: {
-          decimalNumbers: true,
+          ssl: process.env.DB_SSL === 'true' ? { require: true, rejectUnauthorized: false } : false,
         },
       }
     );
